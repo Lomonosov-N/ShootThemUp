75d27e6373cca0997fee15b9e7ec828f38d5419b
nikita.lomonosov <lomonosov.n97@gmail.com>
2023-11-08 19:49:10 +0100

    Логика и анимация бега;  Передвижение персонажа назад, вправо и влево

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -5,9 +5,10 @@
 #include "Camera/CameraComponent.h"
 #include "Components/InputComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Components/STUCharacterMovementComponent.h"
 
 // Sets default values
-ASTUBaseCharacter::ASTUBaseCharacter()
+ASTUBaseCharacter::ASTUBaseCharacter(const FObjectInitializer& ObjInit) : Super(ObjInit.SetDefaultSubobjectClass<USTUCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -44,14 +45,47 @@ void ASTUBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
         PlayerInputComponent->BindAxis("LookUp", this, &ASTUBaseCharacter::AddControllerPitchInput);
         PlayerInputComponent->BindAxis("TurnAround", this, &ASTUBaseCharacter::AddControllerYawInput);
         PlayerInputComponent->BindAction("Jump",IE_Pressed ,this, &ASTUBaseCharacter::Jump);
+        PlayerInputComponent->BindAction("Run",IE_Pressed ,this, &ASTUBaseCharacter::OnStartRunning);
+        PlayerInputComponent->BindAction("Run",IE_Released ,this, &ASTUBaseCharacter::OnStopRunning);
 }
+
+//bool ASTUBaseCharacter::IsRunning() const
+//{
+//        return WantsToRun && IsMovingForward && !GetVelocity().IsZero();
+//}
 
 void ASTUBaseCharacter::MoveForward(float Amount)
 {
+        IsMovingForward = Amount > 0.0f;
         AddMovementInput(GetActorForwardVector(), Amount);
 }
 
 void ASTUBaseCharacter::MoveRight(float Amount)
 {
+        if(Amount == 0.0f) return;
         AddMovementInput(GetActorRightVector(), Amount);
+}
+
+void ASTUBaseCharacter::OnStartRunning()
+{
+        WantsToRun = true;
+}
+void ASTUBaseCharacter::OnStopRunning()
+{
+        WantsToRun = false;
+}
+
+bool ASTUBaseCharacter::IsRunning() const
+{
+        return WantsToRun && IsMovingForward && !GetVelocity().IsZero();
+}
+
+float ASTUBaseCharacter::GetMovementDirection() const
+{
+        if(GetVelocity().IsZero()) return 0.0f;
+        const auto VelocityNormal = GetVelocity().GetSafeNormal();
+        const auto AngleBetween = FMath::Acos(FVector::DotProduct(GetActorForwardVector(),VelocityNormal));
+        const auto CrossProduct = FVector::CrossProduct(GetActorForwardVector(),VelocityNormal);
+        const auto Degrees = FMath::RadiansToDegrees(AngleBetween);
+        return CrossProduct.IsZero() ? Degrees : Degrees * FMath::Sign(CrossProduct.Z);
 }
--- Source/ShootThemUp/Public/Player/STUBaseCharacter.h
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -18,7 +18,7 @@ class SHOOTTHEMUP_API ASTUBaseCharacter : public ACharacter
 
 public:
 	// Sets default values for this character's properties
-	ASTUBaseCharacter();
+	ASTUBaseCharacter(const FObjectInitializer& ObjInit);
 
 protected:
 	// Called when the game starts or when spawned
@@ -35,10 +35,24 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+        
+        
+        UFUNCTION(BlueprintCallable, Category = "Movement")
+        bool IsRunning() const;
+        
+        UFUNCTION(BlueprintCallable, Category = "Movement")
+        float GetMovementDirection() const;
+
+        
 
 private:
+        bool WantsToRun = false;
+        bool IsMovingForward = false;
+        
         void MoveForward(float Amount);
         void MoveRight(float Amount);
         void LookUp(float Amount);
         void TurnAround(float Amount);
+        void OnStartRunning();
+        void OnStopRunning();
 };
