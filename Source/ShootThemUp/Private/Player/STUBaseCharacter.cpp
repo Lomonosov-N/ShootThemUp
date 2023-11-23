0d225599b4fe049d3bec0b9963689745400a6a81
nikita.lomonosov <lomonosov.n97@gmail.com>
2023-11-23 18:32:08 +0100

    SpectatingPawn

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -8,6 +8,7 @@
 #include "Components/STUCharacterMovementComponent.h"
 #include "Components/STUHealthComponent.h"
 #include "Components/TextRenderComponent.h"
+#include "GameFramework/Controller.h"
 
 DEFINE_LOG_CATEGORY_STATIC(BaseCharacterLog, All, All);
 
@@ -120,5 +121,10 @@ void ASTUBaseCharacter::OnDeath()
         GetCharacterMovement()->DisableMovement();
 
         SetLifeSpan(5.0f);
+
+        if(Controller)
+        {
+                Controller->ChangeState(NAME_Spectating);
+        }
 }
 
