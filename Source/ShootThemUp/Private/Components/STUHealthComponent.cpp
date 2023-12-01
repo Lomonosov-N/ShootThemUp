9b3a29a57702f54b121880c920fdfffe3755ccb9
nikita.lomonosov <lomonosov.n97@gmail.com>
2023-12-01 15:34:18 +0100

    AutoHeal

--- Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "Components/STUHealthComponent.h"
 #include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "TimerManager.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, All, All);
 
@@ -17,8 +19,7 @@ void USTUHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	Health = MaxHealth;
-        OnHealthChanged.Broadcast(Health);
+	SetHealth(MaxHealth);
 
         AActor* ComponentOwner = GetOwner();
         if (ComponentOwner)
@@ -32,13 +33,34 @@ void USTUHealthComponent::OnTakeAnyDamage(
        AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
 {
 
-        if (Damage <= 0.0f || IsDead()) return;
-        Health = FMath::Clamp(Health - Damage, 0.0f, MaxHealth);
-        OnHealthChanged.Broadcast(Health);
+        if (Damage <= 0.0f || IsDead() || !GetWorld()) return;
+        
+        SetHealth(Health - Damage);
+
+        GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
 
         if (IsDead())
         {
                 OnDeath.Broadcast();
         }
+        else if(AutoHeal)
+        {
+                GetWorld()->GetTimerManager().SetTimer(HealTimerHandle, this, &USTUHealthComponent::HealUpdate, HealUpdateTime, true, HealDelay);
+        }
 }
 
+void USTUHealthComponent::HealUpdate()
+{
+        SetHealth(Health+ HealModifier);
+       
+        if(FMath::IsNearlyEqual(Health, MaxHealth) && GetWorld())
+        {
+                GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
+        }
+}
+
+void USTUHealthComponent::SetHealth(float NewHealth)
+{
+        Health = FMath::Clamp(NewHealth, 0.0f, MaxHealth);
+        OnHealthChanged.Broadcast(Health);
+}
