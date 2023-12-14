31192de31f4b34384cc04994e59af3dc91f6b99d
nikita.lomonosov <lomonosov.n97@gmail.com>
2023-12-14 11:02:41 +0100

    Launcher Projectile Part 1

--- Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
@@ -2,4 +2,18 @@
 
 
 #include "Weapon/STULauncherWeapon.h"
+#include "Weapon/STUProjectile.h"
+#include "Kismet/GameplayStatics.h"
 
+void ASTULauncherWeapon::StartFire()
+{
+        MakeShot();
+}
+
+void ASTULauncherWeapon::MakeShot()
+{
+        const FTransform SpawnTransform(FRotator::ZeroRotator, GetMuzzleWorldLocation());
+       auto Projectile = UGameplayStatics::BeginDeferredActorSpawnFromClass(GetWorld(), ProjectileClass, SpawnTransform);
+
+        UGameplayStatics::FinishSpawningActor(Projectile, SpawnTransform);
+}
